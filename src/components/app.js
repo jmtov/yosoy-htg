c4167c0ecc65eac6c46510c4aa4b80d9844f9a7e
leoromero97 <leonardogerbacio@gmail.com>
2020-03-23 15:56:46 -0300

    agregue topbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Scaner from './Scaner';
 import styles from './styles.module.scss'
+import Topbar from './topbar';
 
 
 function App() {
   return (
     <div className={styles.background}>
+      <Topbar/>
       <Scaner/>
     
     </div>
