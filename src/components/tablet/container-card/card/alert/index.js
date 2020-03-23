b7cc9984ed8fc96f386f750b16662baa73fb29a7
leoromero97 <leonardogerbacio@gmail.com>
2020-03-23 02:55:07 -0300

    actualice el npm

--- src/components/tablet/container-card/card/alert/index.js
+++ src/components/tablet/container-card/card/alert/index.js
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 function Alert () {
     return(
         <div className={styles.alert}>
+            
             <p>Recorda pedir solo la cantidad de pastillas que necesites para que siempre haya medicacion disponible para otre</p>
         </div>
     );
