1b1d3ab1b3972c02750bcce437cae57fbc9b2762
leoromero97 <leonardogerbacio@gmail.com>
2020-03-23 15:42:05 -0300

    puse topbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tablet from './tablet';
+import Topbar from './topbar';
 
 function App() {
   return (
     <div>
+      <Topbar/>
       <Tablet/>
     </div>
   );
--- src/components/tablet/index.js
+++ src/components/tablet/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Topbar from './topbar';
+import Topbar from '../topbar';
 import ContainerCard from './container-card';
 import Button from './button';
 
