0130fd63ee1acfa469d360953eccfcb73b20f205
leoromero97 <leonardogerbacio@gmail.com>
2020-03-23 15:50:04 -0300

    puse navbar

--- src/components/tablet/index.js
+++ src/components/tablet/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Topbar from '../topbar';
 import ContainerCard from './container-card';
-import Button from './button';
+import Button from '../button';
 
 
 class Tablet extends React.Component {
     render() {
         return(
             <div>
-                <Topbar/>
                 <ContainerCard/>
                 <Button/>
             </div>
--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import imgNavbar from "../Assets"
+import imgNavbar from "../assets"
 import Atras from './atras';
 import Logo from './logo';
 
