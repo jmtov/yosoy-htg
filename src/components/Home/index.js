8a6a8932c6bbaee0edc4ef0adcc4a39437a975f1
leoromero97 <leonardogerbacio@gmail.com>
2020-03-25 15:48:19 -0300

    se hicieron los cambios pedidos en git

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ContainerOne from './ContainerOne';
 import styles from './styles.module.scss';
-import ContainerTwo from './ContainerTwo';
+
 
 function Home() {
     return(
         <div className={styles.home}>
-            <ContainerOne/>
+            <div className={styles.cont1}>
+                <div className={styles.containerHome}>
+                    <h1 className={styles.title}>¡Hola!</h1>
+                    <strong>Queremos darte una mano :)</strong><br/>
+                    <strong>¿Cómo podemos ayudarte hoy?</strong>
+                </div>
+            </div>
             <hr className={styles.hr}/>
-            <ContainerTwo/>
+            <div className={styles.cont2}>
+                <div className={styles.containerButton}>
+                <button className={styles.button1}>QUIERO DONAR</button>
+                <button className={styles.button2}>QUIERO PEDIR</button>
+                </div>
+            </div>
         </div>
     );
 }
