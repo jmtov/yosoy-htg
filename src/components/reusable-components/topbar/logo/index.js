9e860e73e5a6eb74f7b40a594dceb9f5002abb7d
leoromero97 <leonardogerbacio@gmail.com>
2020-03-31 18:33:29 -0300

    se eliminaron archivos servikower y test

--- src/components/reusable-components/topbar/logo/index.js
+++ src/components/reusable-components/topbar/logo/index.js
@@ -5,12 +5,10 @@ import styles from './styles.module.scss';
 class Logo extends React.Component {
   render() {
     return (
-      <ul className={styles.ul}>
-        <li>
-          <img src={this.props.assets} className={styles.assets} alt="logo" />
-        </li>
-      </ul>
-    )
+      <div>
+         <img src={this.props.assets} className={styles.assets} alt="logo" />
+      </div>     
+    );
   }
 }
 
