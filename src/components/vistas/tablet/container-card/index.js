110301287a9e998e6df0c4419ff3aceb482f2a59
leoromero97 <leonardogerbacio@gmail.com>
2020-04-01 19:03:38 -0300

    se corrigio codigo y agrego la imagen del medicamento

--- src/components/vistas/tablet/container-card/index.js
+++ src/components/vistas/tablet/container-card/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Card from './card';
+import CardZoom from './card';
 
 class ContainerCard extends React.Component {
-    render() {
-        return (
-            <div className={styles.containerCard}>
-                <Card />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.containerCard}>
+        <CardZoom />
+      </div>
+    );
+  }
 }
 
 export default ContainerCard;
