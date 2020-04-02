654e81ec83227e6a4e3aa500010fc2961e307fb7
leoromero97 <leonardogerbacio@gmail.com>
2020-04-02 03:42:43 -0300

    se modifico y corrigio codigo

--- src/components/vistas/confirmation/index.js
+++ src/components/vistas/confirmation/index.js
@@ -7,8 +7,7 @@ function Confirmation() {
     <div className={styles.cont1}>
       <div className={styles.info}>
         <h1 className={styles.title}>¡Todo listo!</h1>
-        <p className={styles.text}>La Fundacion Hablemos de HIV te está esperando.<br></br>
-          <br></br> Recordá que solo podemos reservar la medicación que pediste durante las proximas 72hs.</p>
+        <p className={styles.text}>La Fundacion Hablemos de HIV te está esperando. Recordá que solo podemos reservar la medicación que pediste durante las proximas 72hs.</p>
         <h2 className={styles.subtitle}>¿Qué tengo que llevar?</h2>
         <p className={styles.text}>Solo necesitás presentar tu DNI.</p>
       </div>
