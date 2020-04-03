9b827a8201ba6c8fb1b9becd8a776b0196cc168a
leoromero97 <leonardogerbacio@gmail.com>
2020-04-03 11:18:54 -0300

    se elimino el componente targets

--- src/components/vistas/foundations-selector/contenedor-fundaciones/index.js
+++ src/components/vistas/foundations-selector/contenedor-fundaciones/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import Targets from './targets';
+import Cards from './cards';
 import styles from './styles.module.scss';
 
 class ContainerFundaciones extends React.Component {
-
   render() {
     return (
       <div>
         <h4 className={styles.titleContainer}>{this.props.title}</h4>
-        <form className={styles.contTarjetas}>
-          <Targets />
-          <Targets />
-          <Targets />
-          <Targets />
-          <Targets />
-          <Targets />
-          <Targets />
-          <Targets />
+        <form className={styles.contCards}>
+          <Cards titleFoundation="HABLEMOS DE HIV" />
+          <Cards titleFoundation="HELIOS SALUD"/>
+          <Cards titleFoundation="HABLEMOS DE HIV"/>
+          <Cards titleFoundation="HELIOS SALUD"/>
+          <Cards titleFoundation="FUNDACIÓN HUESPED"/>
+          <Cards titleFoundation="FUNDACIÓN ROSAS"/>
+          <Cards titleFoundation="FUNDACIÓN HUESPED"/>
+          <Cards titleFoundation="FUNDACIÓN ROSAS"/>
         </form>
       </div>
     );
