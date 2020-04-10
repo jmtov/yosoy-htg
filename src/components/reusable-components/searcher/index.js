d09189bad8d5712126f8cc74fa45dbed580b9d52
leoromero97 <leonardogerbacio@gmail.com>
2020-04-09 21:00:22 -0300

    se empezo la vista confirmacion en mobile

--- src/components/reusable-components/searcher/index.js
+++ src/components/reusable-components/searcher/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Form from './form';
+import lupa from '../../../assets/lupa.png';
 
 class Searcher extends React.Component {
-    render() {
-        return (
-            <div className={styles.contBuscador}>
-                <Form placeholder={this.props.placeholder} />
-            </div>
-        );
-    }
+  render() {
+    return (
+     <form action="" className={styles.containerSearcher}>
+        <button className={styles.containerIcon} type='submit'>
+          <img src={lupa} className={styles.icon} alt="lupa" />
+        </button>
+        <div className={styles.containerInput}>
+          <input type="text" placeholder={this.props.placeholder} className={styles.input} />
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Searcher;
--- src/components/screens/desktop/confirmation/index.js
+++ src/components/screens/desktop/confirmation/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
 import Home from '../home';
+import logoBlue from '../../../../assets/logoBlue.png';
+import closeBlue from '../../../../assets/closeBlue.png';
 
 function Confirmation() {
   return (
     <div className={styles.contenedorPrincipal}>
+      <nav className={styles.nav}>
+        <button className={styles.buttonClose}>
+          <img src={closeBlue} className={styles.closeBlue} alt="close" />
+        </button>
+        <div>
+          <img src={logoBlue} className={styles.logoBlue} alt="logo" />
+        </div>
+      </nav>
       <div className={styles.containerText}>
         <h1 className={styles.title}>¡Todo listo!</h1>
-        <p>La Fundacion Hablemos de HIV te está esperando.</p>
+        <p className={styles.text}>La Fundacion Hablemos de HIV te está esperando.</p>
         <p className={styles.text}> Recordá que solo podemos reservar la medicación que pediste durante las proximas 72hs.</p>
         <h2 className={styles.subtitle}>¿Qué tengo que llevar?</h2>
         <p className={styles.text}>Solo necesitás presentar tu DNI.</p>
@@ -20,7 +30,7 @@ function Confirmation() {
         </button>
       </Link>
       <footer className={styles.footer}>
-        <hr className={styles.hrFoot} />
+        <hr className={styles.hrFooterEnd} />
         <p className={styles.textFooter}>Copyright © 2019 YOSOY. Todos los derechos reservados.</p>
       </footer>
     </div>
